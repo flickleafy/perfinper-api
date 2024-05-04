a1c327c3d01cb3de1f965df7d366b8f7bf850c69
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-05-03 20:42:44 -0400

    update transaction model

--- src/models/TransactionModel.js
+++ src/models/TransactionModel.js
@@ -1,17 +1,31 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-let schema = mongoose.Schema({
-  description: String,
-  value: Number,
-  category: String,
-  year: Number,
-  month: Number,
-  day: Number,
-  yearMonth: String,
-  yearMonthDay: String,
-  type: String,
+const transactionSchema = mongoose.Schema({
+  transactionDate: { type: Date, default: Date.now },
+  transactionPeriod: String,
+  totalValue: String,
+  individualValue: String,
+  freightValue: String,
+  itemName: String,
+  itemDescription: String,
+  itemUnits: Number,
+  transactionLocation: String,
+  transactionType: { type: String, enum: ['credit', 'debit'] },
+  transactionCategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'category',
+  },
+  groupedItem: Boolean,
+  groupedItemsReference: String, // Shared Reference
+  transactionFiscalNote: String,
+  transactionId: String,
+  transactionStatus: String,
+  companyName: String,
+  companySellerName: String,
+  companyCnpj: String,
+  transactionOrigin: String,
 });
 
-const TransactionModel = mongoose.model('transaction', schema);
+const TransactionModel = mongoose.model('transaction', transactionSchema);
 
-module.exports = TransactionModel;
+export default TransactionModel;
