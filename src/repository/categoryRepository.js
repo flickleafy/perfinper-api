fb4f90c50d57d48d7d630b42db282e4547169630
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-05-04 13:12:15 -0400

    enhance error handling

--- src/repository/categoryRepository.js
+++ src/repository/categoryRepository.js
@@ -2,14 +2,10 @@ import CategoryModel from '../models/CategoryModel.js';
 
 export async function findAll() {
   try {
-    const categories = await CategoryModel.aggregate([
-      { $project: { id: '$_id', _id: 0, name: 1, iconName: 1 } },
-      { $sort: { name: 1 } },
-    ]);
+    const categories = await CategoryModel.find({}).sort({ name: 1 });
     return categories;
   } catch (error) {
-    // Handle or log the error appropriately
-    console.error('Error finding all categories:', error);
+    console.error('Error finding all categories:', error.message);
     throw new Error('Failed to retrieve categories.');
   }
 }
@@ -18,29 +14,27 @@ export async function deleteById(id) {
   try {
     const result = await CategoryModel.findByIdAndDelete(id);
     if (!result) {
-      throw new Error('No category found with given ID.');
+      throw new Error('No category found with the provided ID.');
     }
     return result;
   } catch (error) {
-    console.error('Error deleting category by ID:', error);
-    throw error; // Re-throw the error to be handled by the caller
+    console.error('Error deleting category by ID:', error.message);
+    throw new Error('Failed to delete category.');
   }
 }
 
-export async function updateById(id, newCategoryObject) {
+export async function updateById(id, categoryObject) {
   try {
-    const category = await CategoryModel.findByIdAndUpdate(
-      id,
-      newCategoryObject,
-      { new: true }
-    );
+    const category = await CategoryModel.findByIdAndUpdate(id, categoryObject, {
+      new: true,
+    });
     if (!category) {
-      throw new Error('No category found with given ID.');
+      throw new Error('No category found with the provided ID.');
     }
     return category;
   } catch (error) {
-    console.error('Error updating category by ID:', error);
-    throw error;
+    console.error('Error updating category by ID:', error.message);
+    throw new Error('Failed to update category.');
   }
 }
 
@@ -48,22 +42,22 @@ export async function findById(id) {
   try {
     const category = await CategoryModel.findById(id);
     if (!category) {
-      throw new Error('No category found with given ID.');
+      throw new Error('No category found with the provided ID.');
     }
     return category;
   } catch (error) {
-    console.error('Error finding category by ID:', error);
-    throw error;
+    console.error('Error finding category by ID:', error.message);
+    throw new Error('Failed to find category.');
   }
 }
 
-export async function insert(categoryPrototype) {
+export async function insert(categoryObject) {
   try {
-    const category = new CategoryModel(categoryPrototype);
+    const category = new CategoryModel(categoryObject);
     await category.save();
     return category;
   } catch (error) {
-    console.error('Error inserting new category:', error);
+    console.error('Error inserting new category:', error.message);
     throw new Error('Failed to insert new category.');
   }
 }
