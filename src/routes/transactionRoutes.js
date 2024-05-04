34aff382c163637cf1f2e85581d1834d8fe1864f
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-05-03 22:09:53 -0400

    import fix

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import transactionService from '../services/transactionService.js';
+import * as transactionService from '../services/transactionService.js';
 
 const transactionRouter = express.Router();
 // ?period=2019-03
