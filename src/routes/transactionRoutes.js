adcf2f92a5f8487e1caf73344fad4752dcb84bfd
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-05-12 01:40:22 -0400

    refactor route files

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,21 +1,31 @@
 import express from 'express';
-import * as transactionService from '../services/transactionService.js';
+import {
+  insertTransaction,
+  findTransactionById,
+  updateTransactionById,
+  deleteTransactionById,
+  findAllTransactionsInPeriod,
+  removeAllTransactionsInPeriod,
+  findUniquePeriods,
+  findUniqueYears,
+} from '../services/transactionService.js';
 
 const transactionRouter = express.Router();
 // ?period=2019-03
-transactionRouter.post('/', transactionService.insertTransaction);
-transactionRouter.get('/:id', transactionService.findTransactionById);
-transactionRouter.put('/:id', transactionService.updateTransactionById);
-transactionRouter.delete('/:id', transactionService.deleteTransactionById);
+transactionRouter.post('/', insertTransaction);
+transactionRouter.get('/:id', findTransactionById);
+transactionRouter.put('/:id', updateTransactionById);
+transactionRouter.delete('/:id', deleteTransactionById);
 
 transactionRouter.get(
   '/period/:transactionPeriod',
-  transactionService.findAllTransactionsInPeriod
+  findAllTransactionsInPeriod
 );
 transactionRouter.delete(
   '/period/:transactionPeriod',
-  transactionService.removeAllTransactionsInPeriod
+  removeAllTransactionsInPeriod
 );
-transactionRouter.post('/periods', transactionService.findUniquePeriods);
+transactionRouter.post('/periods', findUniquePeriods);
+transactionRouter.post('/years', findUniqueYears);
 
 export default transactionRouter;
