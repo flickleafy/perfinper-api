358536f46b3524fa2f329a2c49924e61285def54
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-06-02 02:18:21 -0400

    date management update

--- src/infrasctructure/date/index.js
+++ src/infrasctructure/date/index.js
@@ -16,11 +16,7 @@ export function checkSingleDigit(number) {
 export function convertDateToJsDate(dateString) {
   // Split the date string into day, month, and year components
   const [day, month, year] = dateString.split('/');
-
-  // Create a new Date object with year, month (0-indexed), and day
-  const tempDate = new Date(year, month - 1, day);
-  // Adjust the time for GMT-3 by subtracting 3 hours in milliseconds
-  tempDate.setTime(tempDate.getTime() - 3 * 60 * 60 * 1000);
-
-  return tempDate;
+  //
+  const date = new Date(`${year}-${month}-${day}T12:00:00-04:00`);
+  return date;
 }
