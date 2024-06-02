7b4ad983a9c4e40404d7110ab30dcab83d607b14
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-06-02 02:17:47 -0400

    transaction model update

--- src/models/TransactionModel.js
+++ src/models/TransactionModel.js
@@ -2,32 +2,66 @@ import mongoose from 'mongoose';
 
 const transactionSchema = mongoose.Schema({
   transactionDate: { type: Date, default: Date.now },
-  transactionPeriod: String,
-  totalValue: String,
-  individualValue: String,
-  freightValue: String,
-  itemName: String,
-  itemDescription: String,
-  itemUnits: Number,
-  transactionLocation: String,
+  transactionPeriod: String, // month and year of transaction
+  transactionSource: String, // manual, nubank, digio, mercadolivre, flash
+  transactionValue: String,
+  transactionName: String, // brief description/name about the transaction
+  transactionDescription: String, // detailed information about the transaction
+  transactionFiscalNote: String, // fiscal note key
+  transactionId: { type: String, index: true }, // transaction id from the transaction source
+  transactionStatus: { type: String, index: true }, // concluded, refunded, started
+  transactionLocation: {
+    type: String,
+    enum: ['online', 'local', 'other'],
+  },
   transactionType: { type: String, enum: ['credit', 'debit'] },
   transactionCategory: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'category',
+  }, // category id
+  freightValue: String, // only applicable for online transaction of physical product
+  paymentMethod: {
+    type: String,
+    enum: [
+      'money',
+      'pix',
+      'boleto',
+      'debit card',
+      'credit card',
+      'benefit card',
+      'other',
+    ],
   },
-  groupedItem: Boolean,
-  groupedItemsReference: String, // Shared Reference
-  transactionFiscalNote: String,
-  transactionId: String,
-  transactionStatus: String,
-  companyName: String,
-  companySellerName: String,
-  companyCnpj: String,
-  transactionSource: String,
+  items: [
+    {
+      transactionName: String, // brief description/name about the item
+      transactionDescription: String, // detailed information about the item
+      itemValue: String, // individual value of item
+      itemUnits: { type: Number, min: 1 }, // amount of units of the same item
+    },
+  ],
+  companyName: String, // company name
+  companySellerName: String, // seller name from the company
+  companyCnpj: { type: String, index: true }, // company identification key
 });
 
 const transformTransactionFields = (doc, ret, options) => {
   ret.id = ret._id;
+  if (ret.totalValue) {
+    ret.transactionValue = ret.totalValue;
+    delete ret.totalValue;
+  }
+  if (ret.itemDescription) {
+    ret.transactionDescription = ret.itemDescription;
+    delete ret.itemDescription;
+  }
+  if (ret.itemName) {
+    ret.transactionName = ret.itemName;
+    delete ret.itemName;
+  }
+  if (!ret.transactionLocation) {
+    ret.transactionLocation = 'other';
+  }
   delete ret._id; // Delete _id from the response
   delete ret.__v; // Optional: delete version key if not needed
   return ret;
