86c3eb4570b4a73c0add9117e64b8374cfd8a74a
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-06-08 23:42:17 -0400

    date functions update

--- src/infrasctructure/date/index.js
+++ src/infrasctructure/date/index.js
@@ -13,10 +13,42 @@ export function checkSingleDigit(number) {
   return number;
 }
 
-export function convertDateToJsDate(dateString) {
+export function convertDateToJsDate(dateString, timeString = '12:00:00') {
   // Split the date string into day, month, and year components
   const [day, month, year] = dateString.split('/');
   // Set specific timezone and define mid day hour to avoid edge cases
-  const date = new Date(`${year}-${month}-${day}T12:00:00-04:00`);
+  const date = new Date(`${year}-${month}-${day}T${timeString}-04:00`);
   return date;
 }
+
+export const monthsYearThreeLetterAcronymToNumber = {
+  jan: '01',
+  fev: '02',
+  mar: '03',
+  abr: '04',
+  mai: '05',
+  jun: '06',
+  jul: '07',
+  ago: '08',
+  set: '09',
+  out: '10',
+  nov: '11',
+  dez: '12',
+};
+
+export const monthsYearThreeLetterAcronymToExtense = {
+  jan: 'Janeiro',
+  fev: 'Fevereiro',
+  mar: 'Março',
+  abr: 'Abril',
+  mai: 'Maio',
+  jun: 'Junho',
+  jul: 'Julho',
+  ago: 'Agosto',
+  set: 'Setembro',
+  out: 'Outubro',
+  nov: 'Novembro',
+  dez: 'Dezembro',
+};
+
+console.log(months);
